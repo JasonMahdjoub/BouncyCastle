4b9a7076dbbcd5d021affee163f91c2cfdce8804
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2023-01-02 12:29:10 +0100

    configure modules

--- pg/src/main/java/module-info.java
+++ pg/src/main/java/module-info.java
@@ -1,4 +1,20 @@
-module BouncyCastle.pg {
-	requires BouncyCastle.core;
-	requires BouncyCastle.prov;
+module com.distrimind.bouncycastle.pg {
+	requires com.distrimind.bouncycastle.core;
+	requires com.distrimind.bouncycastle.prov;
+	requires java.logging;
+	exports com.distrimind.bouncycastle.apache.bzip2;
+	exports com.distrimind.bouncycastle.gpg.keybox.jcajce;
+	exports com.distrimind.bouncycastle.gpg.keybox.bc;
+	exports com.distrimind.bouncycastle.gpg.keybox;
+	exports com.distrimind.bouncycastle.gpg;
+	exports com.distrimind.bouncycastle.bcpg.attr;
+	exports com.distrimind.bouncycastle.bcpg.sig;
+	exports com.distrimind.bouncycastle.bcpg;
+	exports com.distrimind.bouncycastle.openpgp.bc;
+	exports com.distrimind.bouncycastle.openpgp.jcajce;
+	exports com.distrimind.bouncycastle.openpgp.examples;
+	exports com.distrimind.bouncycastle.openpgp.operator.bc;
+	exports com.distrimind.bouncycastle.openpgp.operator.jcajce;
+	exports com.distrimind.bouncycastle.openpgp.operator;
+	exports com.distrimind.bouncycastle.openpgp;
 }
